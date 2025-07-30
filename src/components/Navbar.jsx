512dcae61606e5c6018fa7bf2c460bb526be84b8
Joshua Kibwage <joshuakibwage23@gmail.com>
2025-07-30 18:00:50 +0300

    feat: add navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
 import { navLinks } from '../constants/index';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+
 
 const Navbar = () => {
+
+  useGSAP(() => {
+    const navTween = gsap.timeline({
+      ScrollTrigger: {
+        trigger: 'nav',
+        start: 'bottom top'
+      }
+    })
+
+    navTween.fromTo('nav', {backgroundColor: 'transparent'}, {
+      backgroundColor: '00000050',
+      backgroundFilter: 'blur(10px)',
+      duration: 1,
+      ease: 'power1.inOut'
+    })
+  })
+
+
   return (
     <nav>
       <div>
